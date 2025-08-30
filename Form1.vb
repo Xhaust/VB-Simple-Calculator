b59c86614c7f6c356dce205fa7f18e491ae7d563
Xhaust <28692487+Xhaust@users.noreply.github.com>
2025-08-30 17:43:14 +0200

    Use CType to cast sender to a button and start to use &= compound assignment operator, which is a good practice.

--- Form1.vb
+++ Form1.vb
@@ -2,6 +2,7 @@
     Dim firstValue, secondValue, result As Double
 
     Private Sub numClick(sender As Object, e As EventArgs) Handles btn7.Click, btn3.Click, btn2.Click, btn5.Click, btn8.Click, btn9.Click, btn6.Click, btn4.Click, btn1.Click, btn0.Click, btnDot.Click
-        txtDisplay.Text = txtDisplay.Text & sender.text
+        Dim btn As Button = CType(sender, Button)
+        txtDisplay.Text &= btn.Text
     End Sub
 End Class
